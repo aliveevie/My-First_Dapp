42528ee563ca2c493cafd0a9cff47d57f0dde926
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-06 13:29:53 +0100

    Update many files

--- interact.js
+++ interact.js
@@ -47,24 +47,59 @@ async function connectWallet(){
     let accountList = await provider.send('eth_requestAccounts', []);
     account = await accountList[0];
     document.getElementById('caccount').innerHTML = 'Current Account is: ' + account;
+	getLearners();
+
 }
-connectWallet();
+
 
 async  function getContract(){
     let signer = provider.getSigner(account);
-    let contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
-    
+    let contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer); 
     return contract;
 }
-getContract();
 
 
 async function getLearners(){
     let contract = await getContract();
-    console.log(contract);
+   
     let learners = await contract.getAllLearners();
-    console.log(learners);
+
+	for(const item of learners){
+			appendCard(item);
+	}
 }
 
+function appendCard(item) {
+	let container = document.getElementsByClassName("container")[0];
+	let card = document.createElement("div");
+	card.className = "card";
+	card.innerHTML =
+	  "Address : " + item.from + "<br/>" + "Learning : " + item.learning;
+	container.append(card);
+  }
 
-getLearners();
+  
+
+async function addLearner() {
+	let learningtext = document.getElementById("inputText");
+	if (learningtext.value === "") {
+	  learningtext.style.border = "2px solid red";
+	  learningtext.setAttribute("placeholder", "This filed can not be blank");
+	  return;
+	}
+	let contract = await getContract();
+	console.log(contract);
+	let txn = await contract.addLearner(learningtext.value);
+	let showhash = document.getElementById("txnhash");
+	let a = document.createElement("a");
+	a.href = `https://goerli.etherscan.io/tx/${txn.hash}`;
+	a.innerHTML = "Follow your transaction here";
+	showhash.append(a);
+	await txn.wait();
+	history.go(0);
+  }
+  
+
+window.addEventListener("load", connectWallet);
+
+
